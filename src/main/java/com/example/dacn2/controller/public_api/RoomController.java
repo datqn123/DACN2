74baa4333bfd5372ce18d869716d59db17852a7b
datne123 <datmotnhit@gmail.com>
2025-12-08 10:22:49 +0700

    feat: Implement initial public APIs, services, and entities for home, location, hotel, voucher, and booking functionalities.

--- src/main/java/com/example/dacn2/controller/public_api/RoomController.java
+++ src/main/java/com/example/dacn2/controller/public_api/RoomController.java
@@ -5,11 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dacn2.dto.ApiResponse;
 import com.example.dacn2.entity.hotel.Room;
 import com.example.dacn2.service.entity.RoomService;
 
+@RestController
+@RequestMapping("/api/public/rooms")
 public class RoomController {
     @Autowired
     private RoomService roomService;
